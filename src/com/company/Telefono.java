4e03644a5c234f47aa2a979fe33cd9ccf3f76aae
Gabs343 <gabriellopezp343@gmail.com>
2020-12-28 13:13:01 -0300

    clase telefono con atributos y metodo

--- src/com/company/Telefono.java
+++ src/com/company/Telefono.java
@@ -1,4 +1,40 @@
 package com.company;
 
 public class Telefono {
+
+    private int bateria;
+    private boolean encendido;
+    private boolean conexionUsb;
+    //Interfaz interfaz;
+    //Memoria memorias = new Memoria[1];
+
+    public void Encender(){}
+
+    public void Apagar(){}
+
+    public void AñadirMemoriaExterna(){}
+
+    public void ExtraerMemoriaExterna(){}
+
+    public void Conectar(){}
+
+    public void ConectarADispositivo(){}
+
+    public void Desconectar(){}
+
+    private void Cargar(){}
+
+    private void Descargar(){}
+
+    public int getBateria() {
+        return bateria;
+    }
+
+    public boolean isConexionUsb() {
+        return conexionUsb;
+    }
+
+    public boolean isEncendido() {
+        return encendido;
+    }
 }
