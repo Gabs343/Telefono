dc5181d7c5d1b0fae623b248be75117b47038a4e
Gabs343 <gabriellopezp343@gmail.com>
2020-12-29 15:37:55 -0300

    clase Memoria creada

---
+++ src/com/company/Memoria.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.ArrayList;
+
+public class Memoria {
+    private int almacenamiento;
+    //ArrayList<App> apps = new ArrayList<App>();
+
+    public Memoria(int almacenamiento){
+        almacenamiento = this.almacenamiento;
+    }
+
+    public void Almacenar(){}
+}
