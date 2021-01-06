658a3a75db8b9f5136f9d75871e473dd6e9ab781
Gabs343 <gabriellopezp343@gmail.com>
2021-01-05 22:52:29 -0300

    cambios pequeños

--- src/com/company/Inventario.java
+++ src/com/company/Inventario.java
@@ -6,7 +6,9 @@ public class Inventario {
     private ArrayList<Item> inventario = new ArrayList<Item>();
 
     public void Añadir(Item item){
+
         inventario.add(item);
+        System.out.println("Se ha añadido un item a tu invetario");
     }
 
 }
