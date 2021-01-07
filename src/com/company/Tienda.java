5d88f6aa6781738a53c54e9681c594c33402628e
Gabs343 <gabriellopezp343@gmail.com>
2021-01-06 23:36:04 -0300

    creando la clase tienda

---
+++ src/com/company/Tienda.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Tienda extends App{
+
+    ArrayList<App> apps = new ArrayList<App>();
+
+    public Tienda(int peso, String nombre) {
+        super(peso, nombre);
+    }
+
+    @Override
+    public void Ejecutar() {
+
+    }
+
+    @Override
+    public void Cerrar() {
+
+    }
+
+
+}
