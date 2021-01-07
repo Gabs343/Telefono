cc3bff7ffee202315ac606935bb9f9e5949c93ff
Gabs343 <gabriellopezp343@gmail.com>
2021-01-06 23:34:31 -0300

    metodo almacenar añadido

--- src/com/company/Memoria.java
+++ src/com/company/Memoria.java
@@ -3,11 +3,19 @@ import java.util.ArrayList;
 
 public class Memoria implements Item{
     private int almacenamiento;
-    //ArrayList<App> apps = new ArrayList<App>();
+    ArrayList<App> apps = new ArrayList<App>();
 
     public Memoria(int almacenamiento){
         this.almacenamiento = almacenamiento;
     }
 
-    public void Almacenar(){}
+    public void Almacenar(App app){
+        apps.add(app);
+        almacenamiento -= app.getPeso();
+    }
+
+    public void Borrar(App app){
+        apps.remove(app);
+        almacenamiento += app.getPeso();
+    }
 }
